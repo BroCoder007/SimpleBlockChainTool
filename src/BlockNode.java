998239377bbc92a09ece8c5085d0030b35a203b7
BroCoder007 <right2abhinavrs@gmail.com>
2025-07-30 11:54:00 +0530

    Fixed minor errors

--- src/BlockNode.java
+++ src/BlockNode.java
@@ -1,13 +1,13 @@
 public class BlockNode {
-        String bId;
-        String bData; //blockData
-        String bType; //blockType
+        String blockId;
+        String blockData; //blockData
+        String blockType; //blockType
         BlockNode prev;
         BlockNode next;
 
         BlockNode(String id, String data, String type) {
-            this.bId = id;
-            this.bData = data;
-            this.bType = type;
+            this.blockId = id;
+            this.blockData = data;
+            this.blockType = type;
         }
     }
--- src/SimpleBlockchain.java
+++ src/SimpleBlockchain.java
@@ -194,7 +194,7 @@ public class SimpleBlockchain {
      //
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Blockchain chain = new Blockchain();
+        BlockchainLedger chain = new BlockchainLedger();
 
         int n = Integer.parseInt(sc.nextLine());
         for (int i = 0; i < n; i++) {
